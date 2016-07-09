6881ab258b0af5d82eb37aa87268a4e12b17a72c
Erin Bennett <erindb@stanford.edu>
2016-07-08 17:46:25 -0700

    more floundering

--- models/sample-logistic.R
+++ models/sample-logistic.R
@@ -19,15 +19,15 @@ expand.grid(
   x1=seq(a,b,length.out=n.grid),
   x2=seq(a,b,length.out=n.grid)) %>%
   mutate(
-    ydet = m + b1*x1 + b2*x2,
-    y = mapply(function(m,s) {return(rnorm(1, mean=m, sd=s))}, ydet, sigma),
+    y = m + b1*x1 + b2*x2,
+    # y = mapply(function(m,s) {return(rnorm(1, mean=m, sd=s))}, ydet, sigma),
     probs = sigmoid(y),
     labels = sapply(probs, function(p) {return(rbinom(1,1,p))})) %>%
   ggplot(., aes(x1, x2, fill=labels, colour=probs)) +
   geom_tile() +
   theme_few()
 
-n = 20
+n = 10
 data = data.frame(
   x1 = runif(n, min=a, max=b),
   x2 = runif(n, min=a, max=b),
@@ -39,10 +39,11 @@ data = data.frame(
     label = (sapply(probs, function(p) {return(rbinom(1,1,p))})==1))
 ggplot(data, aes(x1, x2, colour=label, shape=set)) +
   geom_point(size=3) +
+  geom_text(aes(label=1:nrow(data)), hjust=-0.8) +
   theme_few(base_size = 18) +
   scale_colour_brewer(palette = "Paired")
 ggsave('logistic-data-plot.png', width=10, height=6)
 
 data %>%
   select(x1, x2, label, set) %>%
-  write.csv(., row.names = F, col.names = T, file = 'logistic-data.csv')
+  write.csv(., row.names = F, file = 'logistic-data.csv')
