ec70ace5046b8683cd13cca98aff02d06d85a4fd
Erin Bennett <erindb@stanford.edu>
2016-08-25 10:10:22 -0700

    save old agent-example state

--- models/04-refactoring/agent-example/agent-example.R
+++ models/04-refactoring/agent-example/agent-example.R
@@ -4,13 +4,33 @@ library(tidyr)
 library(dplyr)
 library(jsonlite)
 library(ggthemes)
-theme_new = theme_set(theme_few(base_size = 14))
+
+theme_black = theme_few() + theme(
+  plot.background = element_rect(fill="black", colour="black"),
+  axis.line.x = element_line(colour="lightgray"),
+  axis.line.y = element_line(colour="lightgray"),
+  axis.ticks = element_line(colour="lightgray"),
+  axis.text = element_text(colour="lightgray"),
+  strip.text = element_text(colour="lightgray"),
+  panel.border = element_rect(colour="black"),
+  panel.background = element_rect(fill="black"),
+  strip.background = element_rect(fill="black"),
+  legend.position = "none",
+  axis.title = element_text(colour="lightgray"))
+
+theme.new = theme_set(theme_black)
+
+scale_colour_black = function() {
+  return(scale_colour_hue(c=30, l=30))
+}
+scale_fill_black = function() {
+  return(scale_fill_hue(c=30, l=30))
+}
 
 rs = webppl(
   program_file = "agent-expanded-take2.wppl",
-  inference_opts = list(method="MCMC", samples=5000),
+  inference_opts = list(method="enumerate"),
   model_var = "imagineCounterfactualNoConditioning",
-  output_format = "samples",
   packages = c("./node_modules/jsUtils")
 )
 
@@ -19,10 +39,10 @@ rs %>%
          c(actual.latents.sampleParams.action, cf.latents.sampleParams.action)) %>%
   mutate(world=factor(world, labels=c("actual", "CF"))) %>%
   ggplot(aes(x=sampleParam, colour=world, fill=world)) +
-  geom_histogram(alpha=1/3, position="dodge", binwidth=0.1) +
+  geom_histogram(alpha=1, position="dodge", binwidth=0.1) +
   facet_wrap(~cf.output) +
-  scale_colour_few() +
-  scale_fill_few() +
+  scale_colour_black() +
+  scale_fill_black() +
   ggtitle("sticky sampleParams") +
   ggsave("sticky.sampleParams.png", width=5, height=4)
 
@@ -32,8 +52,8 @@ rs %>% gather("world", "action",
   ggplot(aes(x=action, colour=world, fill=world)) +
   geom_bar() +
   facet_wrap(~ world) +
-  scale_colour_few() +
-  scale_fill_few() +
+  scale_colour_black() +
+  scale_fill_black() +
   theme(axis.text.x = element_text(angle = -20, hjust = 0)) +
   ggtitle("sticky output") +
   ggsave("sticky.output.png", width=5, height=3)
@@ -52,18 +72,17 @@ expl.rs %>%
     labels=c("rationality=1", "should be pretty", "should be yummy"))) %>%
   ggplot(., aes(x=explanation, colour=explanation, fill=explanation, y=prob)) +
   geom_bar(stat="identity") +
-  scale_colour_few() +
-  scale_fill_few() +
   theme(axis.text.x = element_text(angle = -20, hjust = 0),
         legend.position="none") +
   ggtitle("cupcakes explanation") +
+  scale_colour_black() +
+  scale_fill_black()
   ggsave("best.explanation.111.cupcakes.png", width=5, height=4)
 
 expl.rs.brownies = webppl(
   program_file = "agent-expanded-take2-actual-is-brownies.wppl",
-  inference_opts = list(method="MCMC", samples=1000),
+  inference_opts = list(method="enumerate"),
   model_var = "explanationModel",
-  output_format = "samples",
   packages = c("./node_modules/jsUtils")
 )
 
@@ -72,22 +91,36 @@ expl.rs.brownies %>%
     support,
     levels=c("rationality", "utilityCoefs[\"prettiness\"]", "utilityCoefs[\"yumminess\"]"),
     labels=c("rationality=1", "should be pretty", "should be yummy"))) %>%
-  ggplot(., aes(x=explanation, colour=explanation, fill=explanation)) +
-  geom_bar(stat="count") +
-  scale_colour_few() +
-  scale_fill_few() +
+  ggplot(., aes(x=explanation, colour=explanation, fill=explanation, y=prob)) +
+  geom_bar(stat="identity") +
+  scale_colour_black() +
+  scale_fill_black() +
   theme(axis.text.x = element_text(angle = -20, hjust = 0),
         legend.position="none") +
-  ggtitle("brownies explanation") +
+  ggtitle("brownies explanation")
   ggsave("best.explanation.111.brownies.png", width=5, height=4)
 
-expl.rs.flowers = webppl(
-  program_file = "agent-expanded-take2-actual-is-flowers.wppl",
-  inference_opts = list(method="MCMC", samples=1000),
-  model_var = "explanationModel",
-  output_format = "samples",
-  packages = c("./node_modules/jsUtils")
-)
+  expl.rs.flowers = webppl(
+    program_file = "agent-expanded-take2-actual-is-flowers.wppl",
+    inference_opts = list(method="enumerate"),
+    model_var = "explanationModel",
+    packages = c("./node_modules/jsUtils")
+  )
+
+  
+  expl.rs.flowers %>%
+    mutate(explanation=factor(
+      support,
+      levels=c("rationality", "utilityCoefs[\"prettiness\"]", "utilityCoefs[\"yumminess\"]"),
+      labels=c("rationality=1", "should be pretty", "should be yummy"))) %>%
+    ggplot(., aes(x=explanation, colour=explanation, fill=explanation, y=prob)) +
+    geom_bar(stat="identity") +
+    scale_colour_black() +
+    scale_fill_black() +
+    theme(axis.text.x = element_text(angle = -20, hjust = 0),
+          legend.position="none") +
+    ggtitle("flowers explanation")
+  ggsave("best.explanation.111.flowers.png", width=5, height=4)  
 
 expl.rs.flowers %>%
   mutate(explanation=factor(
