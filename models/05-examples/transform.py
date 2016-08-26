32b5f222398ee7bd5940f7c9c19f38c834cdc315
Erin Bennett <erindb@stanford.edu>
2016-08-26 11:06:43 -0700

    (hacky) autoexpander

--- models/05-examples/transform.py
+++ models/05-examples/transform.py
@@ -1,6 +1,7 @@
 import sys
 import warnings
 import re
+import json
 
 def get_tag():
 	if len(sys.argv) > 1:
@@ -17,8 +18,11 @@ tag = get_tag()
 directory = tag + "/"
 cfprior_file = directory + tag + "-cfprior.wppl"
 prog_file = directory + tag + "-program.wppl"
+expressions_file = directory + tag + "-expressions.json"
+obs_file = directory + tag + "-observations.wppl"
+expanded_file = directory + tag + "-autoexpanded.wppl"
 
-def transform_program(prog_file, cfprior_file):
+def transform_program(prog_file, cfprior_file, expressions_file):
 	# for each structure VARNAME with a cfprior,
 	# replace it in the program with 
 	# latents.VARNAME
@@ -53,23 +57,18 @@ def transform_program(prog_file, cfprior_file):
 		})""", prog_maker)
 	warnings.warn("ERP samples other than flip not implemented", Warning)
 
-	# var expressions = {
-	# 	"bacon": bacon,
-	# 	"smokeAlarm": smokeAlarm,
-	# 	"neighborsAngry": neighborsAngry
-	# };
-	# return {
-	# 	expressionValues: expressions,
-	# 	output: {
-	# 		bacon: bacon,
-	# 		smokeAlarm: smokeAlarm,
-	# 		neighborsAngry: neighborsAngry
-	# 	}
-	# };
+	expressions_json = json.loads(open(expressions_file).read())
+	expressions_lst = expressions_json["expressions"]
+	expressions = ",\n\t\t\t\t".join(map(lambda expr: '"' + expr + '": ' + expr, expressions_lst))
 
-	warnings.warn("expressions not implemented", Warning)
+	new_prog = re.sub(r"\n\t\treturn ({(?:.*\n)*\t\t})", r"""
+		return {
+			expressions: {\n\t\t\t\t""" + expressions + r"""
+			},
+			output: \1
+		};""", exogenized_prog)
 
-	return exogenized_prog
+	return new_prog
 
 def write_cfpriors(prog_file, cfprior_file):
 	# last line of file is just for returnify
@@ -81,31 +80,33 @@ def write_cfpriors(prog_file, cfprior_file):
 	cfprior_end = """var sampleParamsPrior = function() {
 	return {
 	""" + "\t" + ",\n\t\t".join(map(lambda variable: variable + ": uniform(0,1)", sampled_values)) + """
-	};""" + "\n};" + "\n\n" + """var cfPrior = function() {
-	return {
-		input: inputPrior(),
-		structureParams: structureParamsPrior(),
-		sampleParams: sampleParamsPrior()
-	};""" + "\n};\n"
+	};""" + "\n};" + "\n"
 	warnings.warn("ERP samples other than flip not implemented", Warning)
 	return cfpriors + cfprior_end
 
-print write_cfpriors(prog_file, cfprior_file)
-print transform_program(prog_file, cfprior_file)
+def write_observations(obs_file):
+	return "".join(open(obs_file).readlines()[:-1])
 
-warnings.warn("observations not implemented", Warning)
+def expand_program(prog_file, cfprior_file, expressions_file, obs_file):
+	start_prog = open("transform-start.wppl").read()
+	end_prog = open("transform-end.wppl").read()
+	cf_comment = "// ------------ CF prior -------------------\n"
+	cf_content = write_cfpriors(prog_file, cfprior_file)
+	prog_comment = "// ------------ make program -------------------\n"
+	prog_content = transform_program(prog_file, cfprior_file, expressions_file)
+	obs_comment = "\n"
+	obs_content = write_observations(obs_file)
+	return "\n".join([
+		start_prog,
+		cf_comment,
+		cf_content,
+		prog_comment,
+		prog_content,
+		obs_comment,
+		obs_content,
+		end_prog
+	])
 
-
-
-
-
-# flip(bacon ? 0.9 : 0);
-# 	var smokeAlarmERP = Bernoulli({
-# 		p: bacon ? 0.9 : 0
-# 	});
-# 	var smokeAlarm = stickySample({
-# 		erp: smokeAlarmERP,
-# 		erpLabel: "smokeAlarm",
-# 		currentLatents: currentLatents,
-# 		origLatents: origLatents
-# 	});
+open(expanded_file, "w").write(
+	expand_program(prog_file, cfprior_file, expressions_file, obs_file) 
+)
